9e456d7cc324fb94e00645142897dc83e8bf47bb
RUFAT RESUL <rufatresul997@gmail.com>
2023-10-19 20:43:20 +0400

    hotel_one

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 
 class Backdrop{
-    constructor(){
+    constructor(clickHandler){
         this.backdropEl = document.querySelector('.backdrop')
+        this.backdropEl.onclick = clickHandler
     }
     open(){
         this.backdropEl.classList.add('backdrop-open')
@@ -11,46 +12,187 @@ class Backdrop{
     }
 
 }
-
-
-class AddTableModal{
-    constructor(parent){
-        this.parent = parent
+class Modal{
+    constructor(className){
+        this.className = className
         this.backdrop = new Backdrop()
-        this.addModalEl = document.querySelector('.add-room-modal')
-        document.querySelector('.add-room-modal').onclick = this.closeModal;
-        this.newRoomButton = document.querySelector('.new-room-button')
-        this.newRoomButton.onclick = this.openModal
+        this.modal = document.querySelector('.' + className)
+        this.xButtonEl = this.modal.lastElementChild
+        this.xButtonEl.onclick = this.closeModal
+        this.exitButtonEl = this.modal.lastElementChild.previousElementSibling.lastElementChild
+        this.exitButtonEl.onclick =this.closeModal
+        this.backdrop = new Backdrop(this.closeModal)
+
     }
     openModal = () =>{
         this.backdrop.open()
-        this.addModalEl.classList.add('modal-open')
+        this.modal.classList.add('modal-open')
     };
     closeModal = () =>{
         this.backdrop.close()
-        this.addModalEl.classList.remove('modal-open')
+        this.modal.classList.remove('modal-open')
        
     };
+}
+class FormItem{
+    constructor(element){
+        this.element = element
+        this.errorElement = this.element.nextElementSibling
+        this.hasError = false
+    }
+    setError = (message) =>{
+        this.element.classList.add('error')
+        this.errorElement.textContent = message
+        this.hasError = true
+    }
+    clearError = ()=>{
+        this.element.classList.remove('error')
+        this.errorElement.textContent = ''
+        this.hasError =  false
 
+    }
+    get hasValue() {
+        return Boolean(this.element.value)
+    }
+    
+}
+
+class InputController extends FormItem{
+    constructor(element,changeHandler){
+        super(element)
+        this.element.oninput = changeHandler
+    }
+    
+    
+}
+
+class SelectController extends FormItem{
+    constructor(element){
+        super(element)
+        this.element.onchange = this.changeHandler
+    }
+    changeHandler = () =>{
+        const selectValue = this.element.value
+        this.value = selectValue
+    }
+}
+class PositiveNumberInputController extends InputController{
+    constructor(element){
+        super(element)
+        this.element.oninput = this.changeHandler
+        this.value = ''
+        
+    }
+    changeHandler = () =>{
+        const newValue = this.element.value
+        if(/^\d*$/.test(newValue)){
+            this.value = newValue
+            this.clearError()
+        }else{
+            this.element.value = this.value
+            this.setError('sadece reqem girilmelidir')
+        }
+    }
+}
+
+
+class AddRoomModal extends Modal{
+    constructor(parent){
+        super('add-room-modal')
+        this.parent = parent
+        this.newRoomButton = document.querySelector('.new-room-button')
+        this.newRoomButton.onclick = this.openModal
+        this.form = document.querySelector('.add-room-modal form')
+        this.numberInput = new PositiveNumberInputController(this.form[0])
+        this.floorInput = new PositiveNumberInputController(this.form[1])
+        this.typeSelect = new SelectController(this.form[2])
+        this.priceInput = new PositiveNumberInputController(this.form[3])
+        this.formItems = [this.numberInput,this.floorInput,this.typeSelect,this.priceInput]
+        this.saveButton = document.querySelector('.add-room-modal .save')
+        this.saveButton.onclick = this.saveHandler
+    }
+    
+    checkValidity = ()=>{
+        for(let formItem of this.formItems){
+            if(formItem.hasError){
+                return false
+            }else if(!formItem.hasValue){
+                formItem.setError('bura bos ola bilmez')
+                return false
+            }
+        }
+        return true
+    }
+    saveHandler = () =>{
+        if(this.checkValidity()){
+            const info ={
+                price:this.priceInput.value,
+                floor:this.floorInput.value,
+                type:this.typeSelect.value,
+                number:this.numberInput.value
+            }
+            this.parent.renderRooms(info)
+            this.saveDataToLocalStorage(info);
+            this.clearInputFields();
+        }else{
+            console.log('bos yer var');
+        }
+    }
+
+    clearInputFields() {
+        this.form.reset();
+    }
+    saveDataToLocalStorage(info) {
+        const existingData = JSON.parse(localStorage.getItem('roomData')) || [];
+        existingData.push(info);
+        localStorage.setItem('roomData', JSON.stringify(existingData));
+    }
+   
+    
 }
 
 class Table{
     constructor(){
         this.tableEl = document.querySelector('.main-table')
         this.tbodyEl = document.querySelector('.main-body')
-        // this.tableModal = new AddTableModal()
-        // this.tableModal.open()
-        // this.addModalEl = document.querySelector('.add-room-modal')
-        // this.backdrop = new Backdrop()
-        this.AddTableModal = new AddTableModal(this)
-    
+        this.addRoomModal = new AddRoomModal(this)
+        this.rooms = this.loadLocalStorage();
+        this.renderRooms();
+        
     }
-    
-    addRoom(){
+ 
+    get getRoomCount(){
+        return this.rooms.length
+    }
 
+    loadLocalStorage(){
+        const roomData=JSON.parse(localStorage.getItem('roomData')) || [];
+        return roomData;
     }
+
+    renderRooms(){
+        this.rooms.forEach((info,index)=>{
+        const roomElement =`
+            <td>${index + 1}</td>
+            <td>${info.number}</td>
+            <td>${info.floor}</td>
+            <td>${info.type}</td>
+            <td><i class="fa-regular fa-circle-check"></i></td>
+            <td>20.08.2023</td>
+            <td>Rufat Rasulov</td>
+            <td>${info.price}AZN</td>
+            <td><i class="fa-regular fa-square-plus button1"></i></td>
+            <td><i class="fa-solid fa-user-minus button2"></i></td>
+            <td><i class="fa-solid fa-trash-can button3"></i></td>
+        `
+        const tr = document.createElement('tr')
+        tr.innerHTML = roomElement
+        this.tbodyEl.appendChild(tr)
+        this.addRoomModal.closeModal()
+    });
 
 }
+}
+ const table = new Table();
 
-const table = new Table()
    
